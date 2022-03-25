1915d80307167ae3b5e92d8b37a6133511a2fe10
Thien Dang <ctap105@gmail.com>
2022-03-25 08:02:30 -0400

    filter 4 stars or higher

--- slash.py
+++ slash.py
@@ -8,8 +8,10 @@ def get_products():
 
 products = [{"product": "Shoes", "price": 35, "rating": 4.2},
 {"product": "White Hat", "price": 21, "rating": 4.8},
-{"product": "Blue Hat", "price": 21, "rating": 3.8}]
+{"product": "Blue Hat", "price": 21, "rating": 3.8},
+{"product": "Brown Shirt", "price": 27, "rating": 4.0}]
 
+def get_four_or_more(products):
+    return [rated for rated in products if rated["rating"] >= 4]
 
-# target = [rated for rated in products if rated["rating"] >=4]
-# print(target)
+print(get_four_or_more(products))
